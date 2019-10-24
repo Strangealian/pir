024c82a231253cc684591f6caa9cedf1aabd31d0
Strangealian <ximwhite@qq.com>
2019-10-24 23:06:23 +0800

    实现数码管动态显示

--- pirtest.asm
+++ pirtest.asm
@@ -1,5 +1,5 @@
 LOOPTIME EQU 60H;
-
+INITONE EQU 03H;
 
 ORG 0000H;
 AJMP MAIN;
@@ -11,6 +11,7 @@ ORG 0060H;
 MAIN:
 MOV 60H,#00H;
     MOV P1,#0FFH;
+    MOV P0,#0FFH;
     MOV P2,#0FFH;
 	MOV A,P2;
     ANL A,#07H; 0  截取三位输入信号，只有输入信号是0，2，4才点亮
@@ -31,64 +32,74 @@ LIGHT:
     MOV DPTR,#TIMETAB;
     MOVC A,@A+DPTR;
     MOV R5,A;R5存放的数据为灯亮的秒数，通过查表得到
-
-    ;点亮数码管
-NUMDIS:
-    MOV R4,#0FEH;先设置十位
-    CLR P2.6;
-    SETB P2.7;
-    MOV P0,R4;
-
-    MOV DPTR,#TIMETAB;
-    MOV A,#04H;
-    MOVC A,@A+DPTR;查表得到相应数字对应值
-    CLR P2.7;
-    SETB P2.6;
-    MOV P0,A;
-    CLR P2.6;
-
-    MOV R4,#0FDH;R4存放数码管位置个位
-    
-    ;MOV R3,A;
-    CLR P2.6;设置选中个位
-    SETB P2.7;
-    MOV P0,R4;
-
-    MOV R3,#03H;存放数码管对应偏移地址
+    MOV R3,#03H;存放数码管个位对应偏移地址
+    MOV R2,#03H;存放数码管十位对应偏移地址
+ 
 LIGHTTIME:
     LCALL DELAY1S;延时1s后数码管显示相应数字
-    CPL P1.1;
-	CPL P1.0;
-
-    MOV DPTR,#TIMETAB;
-    MOV A,R3;
-    MOVC A,@A+DPTR;查表得到相应数字对于值
-    CLR P2.7;
-    SETB P2.6;
-    MOV P0,A;
     INC R3;
-
+    MOV A,#INITONE;
+    ADD A,#9;
+    ;CJNE A,03H,CFG;
+    INC R2;
+CFG:
     DJNZ R5,LIGHTTIME;
 	CPL P1.0;
-   
     AJMP MAIN;
 
 
 
-;延时1s子程序
-DELAY1S:
-    PUSH PSW;
-    PUSH ACC;
-    AJMP DELAY50MS;
-DELAY:
+
+DELAY:;计数器1溢出中断出口
     PUSH PSW;
     PUSH ACC;
     MOV R6,60H;
     INC R6;
     MOV 60H,R6;
-    MOV TH0,#1BH;加一计数器高字节
-    MOV TL0,#5EH;加一计数器低字节
+    MOV TH0,#0E0H;加一计数器高字节
+    MOV TL0,#0C0H;加一计数器低字节
+
+    
+     ;点亮数码管
+NUMDIS:
+   
+    
+    MOV R4,#0FDH;R4存放数码管位置个位
+    
+    ;MOV R3,A;
+    CLR P2.6;位选选中个位
+    SETB P2.7;
+    ;MOV P0,#0FFH;
+    MOV P0,R4;
+    CLR P2.7;
+
+    MOV DPTR,#TIMETAB;
+    MOV A,R3;
+    MOVC A,@A+DPTR;查表得到相应数字对应值
+    SETB P2.6;
+    ;MOV P0,#00H;
+    MOV P0,A;
+    LCALL PWNWAT;延时1ms
+    CLR P2.6;
+
+    MOV R4,#0FEH;
+   
+    SETB P2.7;
+    ;MOV P0,#0FFH;
+    MOV P0,R4;
+    CLR P2.7;
+
+
+    MOV DPTR, #TIMETAB;
+    MOV A,R2;
+    MOVC A,@A+DPTR;
+    SETB P2.6;
+    ;MOV P0,#00H;
+    MOV P0,A;
+    LCALL PWNWAT;
+
     CJNE R6,#14H,BREAK;
+    CLR P2.6;
 	CLR ET1;
     CLR TR1;
     CLR EA;
@@ -100,17 +111,25 @@ BREAK:
     POP PSW;
    	RETI;
 
-;延时50ms子程序 65536-50000=15536=1B5E
+;延时1s子程序
+DELAY1S:
+    PUSH PSW;
+    PUSH ACC;
+    AJMP DELAY50MS;
+;延时50ms子程序 65536-50000=15536=3CB0
 DELAY50MS:
     MOV TMOD,#10H;计数器0工作于方式1
-    MOV TH0,#1BH;加一计数器高字节
-    MOV TL0,#5EH;加一计数器低字节
+    MOV TH0,#0E0H;加一计数器高字节
+    MOV TL0,#0C0H;加一计数器低字节
     SETB EA;
     SETB TR1;
     SETB ET1;
     
 TIMEOUT:  
+  
+
     MOV R6,60H;
+
     CJNE R6,#14H,TIMEOUT;
 	;AJMP LIGHT;
     MOV 60H,#00H;
@@ -118,12 +137,28 @@ TIMEOUT:
     POP PSW;
     RET;
 
-
+PWNWAT: ;power on wait
+    PUSH PSW;
+    PUSH ACC;
+    SETB RS0;
+    MOV R1,#24;
+    MOV R2,#40;
+PW1:
+    NOP;
+    NOP;
+    DJNZ R1,PW1;
+    DJNZ R2,PW1;
+    POP ACC;
+    POP PSW;
+    RET;
 
 
 TIMETAB:
-    DB 03H,14H,1EH;定义灯亮的秒数10，20，30s
-    DB 3FH,06H,5BH,4FH;
-    DB 66H,6DH,7DH,07H;
-    DB 7FH,6FH;
+    DB 0AH,14H,1EH;定义灯亮的秒数10，20，30s
+    ;DB 3FH,06H,5BH,4FH;
+    ;DB 66H,6DH,7DH,07H;
+    ;DB 7FH,6FH;
+    DB 6FH,7FH,07H,7DH;
+    DB 6DH,66H,4FH,5BH;
+    DB 06H,3FH;
 END;
